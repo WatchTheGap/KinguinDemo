4d30a4620d5d43b349aabdfba4d391e457ca4121
Sara Basile <sarabasile@Saras-Air.home>
2018-01-29 16:04:02 -0500

    added company popup sans the waddle of kinguins

--- src/js/popups.js
+++ src/js/popups.js
@@ -9,6 +9,10 @@
     $('#team-popup').toggleClass('hide');
   });
 
+  $('#company').click(function() {
+    $('#company-popup').toggleClass('hide');
+  });
+
   $('.close-popup').click(function() {
     $(this).closest('.popup-outer-wrapper').toggleClass('hide');
   });
