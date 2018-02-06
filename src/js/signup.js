0131066186a2102b394c11edd607dd68c0df77e9
Sara Basile <sarabasile@Saras-Air.home>
2018-02-05 22:32:04 -0500

    added continue buttons for easier navigation to the currency entering forms

--- src/js/signup.js
+++ src/js/signup.js
@@ -15,4 +15,28 @@
     $('#choose-fiat-popup').toggleClass('hide');
   });
 
+  $('#crypto-continue').click(function() {
+    $('#choose-crypto-popup').toggleClass('hide');
+    $('#kyc-popup').toggleClass('hide');
+  });
+
+  $('#fiat-continue').click(function() {
+    $('#choose-fiat-popup').toggleClass('hide');
+    $('#kyc-popup').toggleClass('hide');
+  });
+
+  $('#kyc-continue').click(function() {
+    $('#investor-popup').toggleClass('hide');
+    $('#kyc-popup').toggleClass('hide');
+  });
+
+  $('#investor-button-private').click(function () {
+    $('#investor-private-popup').toggleClass('hide');
+    $('#investor-popup').toggleClass('hide');
+  });
+
+  $('#investor-button-company').click(function () {
+    $('#investor-company-popup').toggleClass('hide');
+    $('#investor-popup').toggleClass('hide');
+  });
 }());
