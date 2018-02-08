576880b551813dd9c1c067431d9f3a230469ad46
Sara Basile <sarabasile@Saras-Air.home>
2018-02-08 11:51:29 -0500

    currency button toggle works, need to replace placeholder images

--- src/js/forms.js
+++ src/js/forms.js
@@ -3,16 +3,33 @@
 
     let currencyType;
 
-    $('#BTC-select').focus(function () {
+    $('#BTC-select').click(function () {
       currencyType = 'BTC';
       localStorage.setItem('currency', currencyType);
       console.log(localStorage);
+      $(this).css('background-image', 'url("img/BTC1.png")')
+        .css('background-color', '#ef5e5e')
+          .closest('.input-wrapper')
+            .css('background-color', '#ef5e5e');
+      $('#ETH-select')
+        .css('background-color', '#d9d9d9')
+          .closest('.input-wrapper')
+            .css('background-color', '#d9d9d9');
     });
-    $('#ETH-select').focus(function () {
+
+    $('#ETH-select').click(function () {
       currencyType = 'ETH';
       localStorage.setItem('currency', currencyType);
       console.log(localStorage);
+      $(this).css('background-image', 'url("img/trees.png")')
+        .css('background-color', '#ef5e5e')
+          .closest('.input-wrapper')
+            .css('background-color', '#ef5e5e');
+      $('#BTC-select').css('background-color', '#d9d9d9')
+          .closest('.input-wrapper')
+            .css('background-color', '#d9d9d9');
     });
+
     $('#EUR-select').focus(function () {
       currencyType = 'EUR';
       localStorage.setItem('currency', currencyType);
