0b507e88c3447447562070f962e1dcebd01b65bc
Sara Basile <sarabasile@Saras-Air.home>
2018-02-15 19:22:29 -0500

    fixed bottom border of why now popup and border color for bottom inactive button.

--- build/js/app.js
+++ build/js/app.js
@@ -255,5 +255,50 @@ $('#roadmap').click(function () {
     $('.why-info-text').find('p').text('interface text is here!!!');
   });
 
+  $('.why-btn:contains("trade")').click(function () {
+    $(this).siblings('.why-active').removeClass('why-active').addClass('why-btn');
+    $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
+    $(this).removeClass('why-btn').addClass('why-active');
+
+    $('.why-info-text').find('p').text('trade text is here!!!');
+  });
+
+  $('.why-btn:contains("technology")').click(function () {
+    $(this).siblings('.why-active').removeClass('why-active').addClass('why-btn');
+    $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
+    $(this).removeClass('why-btn').addClass('why-active');
+
+    $('.why-info-text').find('p').text('technology text is here!!!');
+  });
+
+  $('.why-btn:contains("market")').click(function () {
+    $(this).siblings('.why-active').removeClass('why-active').addClass('why-btn');
+    $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
+    $(this).removeClass('why-btn').addClass('why-active');
+
+    $('.why-info-text').find('p').text('market text is here!!!');
+    });
+
+    $('.last-btn:contains("community")').click(function () {
+      $(this).siblings('.why-active').removeClass('why-active').addClass('why-btn');
+
+      $(this).removeClass('last-btn').addClass('last-active');
+
+      $('.why-info-text').find('p').text('community text is here!!!');
+    });
+
+    $('.why-btn:contains("kinguin")').click(function () {
+      $(this).siblings('.why-active').removeClass('why-active').addClass('why-btn');
+      $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
+      $(this).removeClass('why-btn').addClass('why-active');
+
+      $('.why-info-text').find('p').text('Technology has now reached the point where global trade between gamers can be easy, secure and nearly instant - thanks to blockchain and smart contracts. In just five years, the annual secondary market for digital games has gone from zero to one billion dollars. As new technologies gradually eliminate mistrust from the value chain, we believe the global market for player-to-player trade will dramatically increase.');
+    });
+
+
+
+
+
+
 
 }());
