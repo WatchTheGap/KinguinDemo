4c0a5b83ad68e797869639c475ecf341440043cd
Sara Basile <sarabasile@Saras-Air.home>
2018-02-19 16:36:24 -0500

    read more now properly changes all other collapse buttons to read more when clicked

--- src/js/team.js
+++ src/js/team.js
@@ -3,18 +3,15 @@
 
   $('.team-member a').click(function (e) {
     e.preventDefault();
-    // if ($(this).is(':contains("read more")')) {
-    //   $('.more-info-text').show();
-    // }
-    //
-    // else
+
     if ($(this).is(':contains("collapse")')) {
       $('.more-info-text').hide();
       $(this).text('read more');
+
     } else if ($(this).is(':contains("read more")')) {
+      $('.team-member a:contains("collapse")').text('read more');
       $('.more-info-text').show();
       $(this).text('collapse');
-
     }
   });
 
