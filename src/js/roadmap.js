8459944b3fd15e65ca5e7069722396fc6211caa7
Sara Basile <sarabasile@Saras-Air.home>
2018-02-19 13:10:25 -0500

    fixed border toggle for inactive nodes when a new one is clicked

--- src/js/roadmap.js
+++ src/js/roadmap.js
@@ -4,8 +4,16 @@
   $('.node-wrapper:contains("PROJECT")').click(function () {
     $('#roadmap-footer').find('h2').text('PROJECT START')
       .siblings('p').text('new project text in here!');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
 
+  });
+
+  $('.node-wrapper:contains("ANNOUNCEMENT")').click(function () {
+    $('#roadmap-footer').find('h2').text('ANNOUNCEMENT')
+      .siblings('p').text('ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
 
