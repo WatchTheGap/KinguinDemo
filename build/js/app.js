e1353cf195cc8b3889cf907e43cf329640077dd8
Sara Basile <sarabasile@Saras-Air.home>
2018-02-19 13:57:39 -0500

    fixed header corner shading on ourstory popup, started on info tab content

--- build/js/app.js
+++ build/js/app.js
@@ -78,6 +78,26 @@
 (function() {
   'use strict';
 
+  $('.dates-row li:contains("2013")').click(function () {
+    $(this).addClass('date-active').removeClass('date-inactive');
+    $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+    $('#story-info-text').html('<p>Wowie wow wow</p>');
+    // TODO: this is probably not the best way to do this...
+  });
+
+
+    $('.dates-row li:contains("2014")').click(function () {
+      $(this).addClass('date-active').removeClass('date-inactive');
+      $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+      $('#story-info-text').html('<p>2014</p>');
+      // TODO: this is probably not the best way to do this...
+    });
+
+}());
+
+(function() {
+  'use strict';
+
 // *** About Island ***
 
   $('#bulb').click(function() {
