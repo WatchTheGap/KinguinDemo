8bd9450ec36c6cbc0fbadaf6c1f6e373d4139fea
Sara Basile <sarabasile@Saras-Air.home>
2018-02-19 13:59:29 -0500

    date info tabs ready for content

--- src/js/ourstory.js
+++ src/js/ourstory.js
@@ -9,11 +9,28 @@
   });
 
 
-    $('.dates-row li:contains("2014")').click(function () {
-      $(this).addClass('date-active').removeClass('date-inactive');
-      $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
-      $('#story-info-text').html('<p>2014</p>');
-      // TODO: this is probably not the best way to do this...
-    });
+  $('.dates-row li:contains("2014")').click(function () {
+    $(this).addClass('date-active').removeClass('date-inactive');
+    $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+    $('#story-info-text').html('<p>2014</p>');
+  });
+
+  $('.dates-row li:contains("2015")').click(function () {
+    $(this).addClass('date-active').removeClass('date-inactive');
+    $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+    $('#story-info-text').html('<p>2015</p>');
+  });
+
+  $('.dates-row li:contains("2016")').click(function () {
+    $(this).addClass('date-active').removeClass('date-inactive');
+    $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+    $('#story-info-text').html('<p>2016</p>');
+  });
+
+  $('.dates-row li:contains("2017")').click(function () {
+    $(this).addClass('date-active').removeClass('date-inactive');
+    $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+    $('#story-info-text').html('<p>2017</p>');
+  });
 
 }());
