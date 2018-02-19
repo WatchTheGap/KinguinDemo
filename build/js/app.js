a5169512bdec6ad060d6a8d3f0d239e474929eb7
Sara Basile <sarabasile@Saras-Air.home>
2018-02-19 13:17:55 -0500

    roadmap info tabs are ready for content

--- build/js/app.js
+++ build/js/app.js
@@ -174,8 +174,58 @@
   $('.node-wrapper:contains("PROJECT")').click(function () {
     $('#roadmap-footer').find('h2').text('PROJECT START')
       .siblings('p').text('new project text in here!');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
 
+  });
+
+  $('.node-wrapper:contains("ANNOUNCEMENT")').click(function () {
+    $('#roadmap-footer').find('h2').text('ANNOUNCEMENT')
+      .siblings('p').text('ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("PRE-ICO")').click(function () {
+    $('#roadmap-footer').find('h2').text('PRE-ICO')
+      .siblings('p').text('PRE-ICO PRE-ICO PRE-ICO PRE-ICO PRE-ICO');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("ICO OPENS")').click(function () {
+    $('#roadmap-footer').find('h2').text('ICO OPENS')
+      .siblings('p').text('ICO OPENS ICO OPENS ICO OPENS ICO OPENS ICO OPENS');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("ICO CLOSES")').click(function () {
+    $('#roadmap-footer').find('h2').text('ICO CLOSES')
+      .siblings('p').text('ICO CLOSES ICO CLOSES ICO CLOSES ICO CLOSES ICO CLOSES');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("MARKETPLACE TRANSITIONS")').click(function () {
+    $('#roadmap-footer').find('h2').text('MARKETPLACE TRANSITIONS')
+      .siblings('p').text('MARKETPLACE TRANSITIONS MARKETPLACE TRANSITIONS MARKETPLACE TRANSITIONS');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("COMMUNITY SHARE")').click(function () {
+    $('#roadmap-footer').find('h2').text('COMMUNITY SHARE')
+      .siblings('p').text('COMMUNITY SHARE COMMUNITY SHARE COMMUNITY SHARE');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("TRANSITION END")').click(function () {
+    $('#roadmap-footer').find('h2').text('TRANSITION END')
+      .siblings('p').text('TRANSITION END TRANSITION END TRANSITION END');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
 
--- src/js/roadmap.js
+++ src/js/roadmap.js
@@ -16,5 +16,47 @@
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
+  $('.node-wrapper:contains("PRE-ICO")').click(function () {
+    $('#roadmap-footer').find('h2').text('PRE-ICO')
+      .siblings('p').text('PRE-ICO PRE-ICO PRE-ICO PRE-ICO PRE-ICO');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("ICO OPENS")').click(function () {
+    $('#roadmap-footer').find('h2').text('ICO OPENS')
+      .siblings('p').text('ICO OPENS ICO OPENS ICO OPENS ICO OPENS ICO OPENS');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("ICO CLOSES")').click(function () {
+    $('#roadmap-footer').find('h2').text('ICO CLOSES')
+      .siblings('p').text('ICO CLOSES ICO CLOSES ICO CLOSES ICO CLOSES ICO CLOSES');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("MARKETPLACE TRANSITIONS")').click(function () {
+    $('#roadmap-footer').find('h2').text('MARKETPLACE TRANSITIONS')
+      .siblings('p').text('MARKETPLACE TRANSITIONS MARKETPLACE TRANSITIONS MARKETPLACE TRANSITIONS');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("COMMUNITY SHARE")').click(function () {
+    $('#roadmap-footer').find('h2').text('COMMUNITY SHARE')
+      .siblings('p').text('COMMUNITY SHARE COMMUNITY SHARE COMMUNITY SHARE');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
+  $('.node-wrapper:contains("TRANSITION END")').click(function () {
+    $('#roadmap-footer').find('h2').text('TRANSITION END')
+      .siblings('p').text('TRANSITION END TRANSITION END TRANSITION END');
+    $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
+    $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
+  });
+
 
 }());
