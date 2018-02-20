b7afd65949146ca20a5e300a94f1572a8a405aa2
Sara Basile <sarabasile@Saras-Air.home>
2018-02-20 10:58:01 -0500

    added img border toggle and corrected bio text displays for team popup

--- src/js/team.js
+++ src/js/team.js
@@ -7,12 +7,27 @@
     if ($(this).is(':contains("collapse")')) {
       $('.more-info-text').hide();
       $(this).text('read more');
+      $($(this).parents('.team-member').find('img').removeClass('team-active'));
+
 
     } else if ($(this).is(':contains("read more")')) {
       $('.team-member a:contains("collapse")').text('read more');
       $('.more-info-text').show();
       $(this).text('collapse');
+      $($(this).parents('.team-member').find('img').addClass('team-active'));
+      $($(this).parents('.team-member').siblings().find('.team-active').removeClass('team-active'));
     }
+
+    if($(this).is('#kim-bio')) {
+      $('.more-info-text').text('kim');
+    } else if ($(this).is('#nashat-bio')){
+      $('.more-info-text').text('nashat');
+    } else if ($(this).is('#viktor-bio')) {
+      $('.more-info-text').text('viktor');
+    } else {
+      return;
+    }
+
   });
 
 }());
