8f105fc4295b0c94194f0b07b8bfd96d37a5764a
Sara Basile <sarabasile@Saras-Air.home>
2018-02-21 11:24:03 -0500

    massive reorganization of sass for all popup windows to reduce id use and to make it easier to find everything.

--- build/js/app.js
+++ build/js/app.js
@@ -209,56 +209,56 @@
   'use strict';
 
   $('.node-wrapper:contains("PROJECT")').click(function () {
-    $('#roadmap-footer').find('h2').text('PROJECT START')
+    $('#roadmap-popup .modal-footer').find('h2').text('PROJECT START')
       .siblings('p').text('new project text in here!');
     $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
   $('.node-wrapper:contains("ANNOUNCEMENT")').click(function () {
-    $('#roadmap-footer').find('h2').text('ANNOUNCEMENT')
+    $('#roadmap-popup .modal-footer').find('h2').text('ANNOUNCEMENT')
       .siblings('p').text('ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT ANNOUNCEMENT');
     $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
   $('.node-wrapper:contains("PRE-ICO")').click(function () {
-    $('#roadmap-footer').find('h2').text('PRE-ICO')
+    $('#roadmap-popup .modal-footer').find('h2').text('PRE-ICO')
       .siblings('p').text('PRE-ICO PRE-ICO PRE-ICO PRE-ICO PRE-ICO');
     $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
   $('.node-wrapper:contains("ICO OPENS")').click(function () {
-    $('#roadmap-footer').find('h2').text('ICO OPENS')
+    $('#roadmap-popup .modal-footer').find('h2').text('ICO OPENS')
       .siblings('p').text('ICO OPENS ICO OPENS ICO OPENS ICO OPENS ICO OPENS');
     $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
   $('.node-wrapper:contains("ICO CLOSES")').click(function () {
-    $('#roadmap-footer').find('h2').text('ICO CLOSES')
+    $('#roadmap-popup .modal-footer').find('h2').text('ICO CLOSES')
       .siblings('p').text('ICO CLOSES ICO CLOSES ICO CLOSES ICO CLOSES ICO CLOSES');
     $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
   $('.node-wrapper:contains("MARKETPLACE TRANSITIONS")').click(function () {
-    $('#roadmap-footer').find('h2').text('MARKETPLACE TRANSITIONS')
+    $('#roadmap-popup .modal-footer').find('h2').text('MARKETPLACE TRANSITIONS')
       .siblings('p').text('MARKETPLACE TRANSITIONS MARKETPLACE TRANSITIONS MARKETPLACE TRANSITIONS');
     $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
   $('.node-wrapper:contains("COMMUNITY SHARE")').click(function () {
-    $('#roadmap-footer').find('h2').text('COMMUNITY SHARE')
+    $('#roadmap-popup .modal-footer').find('h2').text('COMMUNITY SHARE')
       .siblings('p').text('COMMUNITY SHARE COMMUNITY SHARE COMMUNITY SHARE');
     $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
   });
 
   $('.node-wrapper:contains("TRANSITION END")').click(function () {
-    $('#roadmap-footer').find('h2').text('TRANSITION END')
+    $('#roadmap-popup .modal-footer').find('h2').text('TRANSITION END')
       .siblings('p').text('TRANSITION END TRANSITION END TRANSITION END');
     $('.node-row').find('.node-active').addClass('node-inactive').removeClass('node-active');
     $(this).find('.roadmap-node').addClass('node-active').removeClass('node-inactive');
@@ -367,11 +367,11 @@
     }
 
     if($(this).is('#kim-bio')) {
-      $('.more-info-text').text('kim');
+      $('#team-popup .more-info-text').text('kim');
     } else if ($(this).is('#nashat-bio')){
-      $('.more-info-text').text('nashat');
+      $('#team-popup .more-info-text').text('nashat');
     } else if ($(this).is('#viktor-bio')) {
-      $('.more-info-text').text('viktor');
+      $('#team-popup .more-info-text').text('viktor');
     } else {
       return;
     }
