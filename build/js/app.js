1f34a9aabfb6d806f4a2db1149e444f5a5ee0b2d
Sara Basile <sarabasile@Saras-Air.home>
2018-02-26 15:43:07 -0500

    added why now tab content. separate class toggle function not working, js remains in blocks for individual tabs.

--- build/js/app.js
+++ build/js/app.js
@@ -455,12 +455,29 @@
 (function() {
   'use strict';
 
+
+  let kinguinReady = "Kinguin was founded on the belief that gamers should be able to freely trade their digital property. This belief is shared by a global community of seven million gamers: by the end of 2017 Kinguin customers will have saved more than $210 million dollars on digital games and in-game items. Now it’s time to evolve the platform to the next generation.";
+
+  let interfaceReady = "Simply put: trade doesn’t have to be boring, tedious or complex. We began work on chat bots more than a year ago and have since served thousands of customers through conversational technology - recording a groundbreaking 96% customer support satisfaction rate. It’s time to revolutionize trade.";
+
+  let tradeReady = "A user-friendly system allows anyone to setup a trade channel and start earning money on digital products or services. Our trade bots will extend deep into the community, facilitating trade on social media platforms, where it will feel as a natural part of any gamer’s lifestyle.";
+
+  let technologyReady = "Technology has now reached the point where global trade between gamers can be easy, secure and nearly instant thanks to blockchain and smart contracts. In just five years, the annual secondary market for digital games has gone from zero to one billion dollars. As new technologies gradually eliminate mistrust from the value chain, we believe the global market for player-to-player trade will dramatically increase.";
+
+  let marketReady = "Online gamers naturally embrace every aspect of digital value creation, micro-transactions and monetization of virtual goods in their games. Our new marketplace will open entirely new revenue streams for the best players, value creators, and influencers - allowing them to monetize a much bigger part of the value creation they facilitate in gaming.";
+
+  let communityReady = "As gaming transitions to a community experience, so should the marketplace of the future. As Kinguin transitions to the blockchain and smart contracts, we’re launching a new micro-service-industry for the global gaming community.";
+
+  // TODO: pull out class toggles into separate function.
+  //       previous attempts failed despite being identical
+  //       to similar function in story popup.
+
   $('.why-btn:contains("interface")').click(function () {
     $(this).siblings('.why-active').removeClass('why-active').addClass('why-btn');
     $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
     $(this).removeClass('why-btn').addClass('why-active');
 
-    $('.why-info-text').find('p').text('interface text is here!!!');
+    $('.why-info-text').find('p').text(interfaceReady);
   });
 
   $('.why-btn:contains("trade")').click(function () {
@@ -468,7 +485,7 @@
     $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
     $(this).removeClass('why-btn').addClass('why-active');
 
-    $('.why-info-text').find('p').text('trade text is here!!!');
+    $('.why-info-text').find('p').text(tradeReady);
   });
 
   $('.why-btn:contains("technology")').click(function () {
@@ -476,7 +493,7 @@
     $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
     $(this).removeClass('why-btn').addClass('why-active');
 
-    $('.why-info-text').find('p').text('technology text is here!!!');
+    $('.why-info-text').find('p').text(technologyReady);
   });
 
   $('.why-btn:contains("market")').click(function () {
@@ -484,7 +501,7 @@
     $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
     $(this).removeClass('why-btn').addClass('why-active');
 
-    $('.why-info-text').find('p').text('market text is here!!!');
+    $('.why-info-text').find('p').text(marketReady);
     });
 
     $('.last-btn:contains("community")').click(function () {
@@ -492,7 +509,7 @@
 
       $(this).removeClass('last-btn').addClass('last-active');
 
-      $('.why-info-text').find('p').text('community text is here!!!');
+      $('.why-info-text').find('p').text(communityReady);
     });
 
     $('.why-btn:contains("kinguin")').click(function () {
@@ -500,7 +517,7 @@
       $(this).siblings('.last-active').removeClass('last-active').addClass('last-btn');
       $(this).removeClass('why-btn').addClass('why-active');
 
-      $('.why-info-text').find('p').text('Technology has now reached the point where global trade between gamers can be easy, secure and nearly instant - thanks to blockchain and smart contracts. In just five years, the annual secondary market for digital games has gone from zero to one billion dollars. As new technologies gradually eliminate mistrust from the value chain, we believe the global market for player-to-player trade will dramatically increase.');
+      $('.why-info-text').find('p').text(kinguinReady);
     });
 
 }());
