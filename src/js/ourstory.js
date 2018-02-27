cd2d622e05a45e92ac886485ceb32175767c4b3a
Sara Basile <sarabasile@Saras-Air.home>
2018-02-27 10:50:51 -0500

    fixed colors/sizing on our story popup, added caret below dates when active, added fixed height to body, aligned text with left most date

--- src/js/ourstory.js
+++ src/js/ourstory.js
@@ -16,27 +16,32 @@
   let sum2017 = 'Team Kinguin Performance Center in Warsaw is initiated. Expected launch in 2018.';
   let end2017 = 'Combined community savings of $150 million on Kinguin.net';
 
-  let activeDate = function activeDate() {
-    $(this).addClass('date-active').removeClass('date-inactive');
-    $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
-  };
-
   $('.dates-row li').click(function () {
     if ($(this).is(':contains("2013")')) {
-      activeDate();
-      $('.year-summary').text(sum2013).siblings('.end-year-sum').text(end2013);
+      $(this).addClass('date-active').removeClass('date-inactive');
+      $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+      $('.year-summary').text(sum2013);
+      $('.end-year-sum').text(end2013);
     } else if ($(this).is(':contains("2014")')) {
-      activeDate();
-      $('.year-summary').text(sum2014).siblings('.end-year-sum').text(end2014);
+      $(this).addClass('date-active').removeClass('date-inactive');
+      $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+      $('.year-summary').text(sum2014);
+      $('.end-year-sum').text(end2014);
     } else if ($(this).is(':contains("2015")')) {
-      activeDate();
-      $('.year-summary').text(sum2015).siblings('.end-year-sum').text(end2015);
+      $(this).addClass('date-active').removeClass('date-inactive');
+      $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+      $('.year-summary').text(sum2015);
+      $('.end-year-sum').text(end2015);
     } else if ($(this).is(':contains("2016")')) {
-      activeDate();
-      $('.year-summary').text(sum2016).siblings('.end-year-sum').text(end2016);
+      $(this).addClass('date-active').removeClass('date-inactive');
+      $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+      $('.year-summary').text(sum2016);
+      $('.end-year-sum').text(end2016);
     } else if ($(this).is(':contains("2017")')) {
-      activeDate();
-      $('.year-summary').text(sum2017).siblings('.end-year-sum').text(end2017);
+      $(this).addClass('date-active').removeClass('date-inactive');
+      $(this).siblings('.date-active').addClass('date-inactive').removeClass('date-active');
+      $('.year-summary').text(sum2017);
+      $('.end-year-sum').text(end2017);
     }
   });
 
