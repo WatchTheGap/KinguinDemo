94df83086f6ad1979f751ea487d73b79e1b4d049
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2018-03-02 14:28:25 -0500

    added countdown timer for billboard

--- build/js/app.js
+++ build/js/app.js
@@ -55,6 +55,39 @@
 (function() {
   'use strict';
 
+  function countdown() {
+
+  			let endTime = new Date("30 March 2018 9:56:00 GMT+00:00");
+  			endTime = (Date.parse(endTime) / 1000);
+
+  			let now = new Date();
+  			now = (Date.parse(now) / 1000);
+
+  			let timeLeft = endTime - now;
+
+  			let days = Math.floor(timeLeft / 86400);
+  			let hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+  			let minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+  			let seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+  			if (hours < "10") { hours = "0" + hours; }
+  			if (minutes < "10") { minutes = "0" + minutes; }
+  			if (seconds < "10") { seconds = "0" + seconds; }
+
+  			$('#ico-days').text(days);
+        $('#ico-hours').text(hours);
+        $('#ico-minutes').text(minutes);
+        $('#ico-seconds').text(seconds);
+
+  	}
+
+  	setInterval(function() { countdown(); }, 1000);
+
+}());
+
+(function() {
+  'use strict';
+
   $('form').submit(function (e) {
     e.preventDefault();
   }); 
