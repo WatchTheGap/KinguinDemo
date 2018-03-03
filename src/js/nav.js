7f4eda607d9f641959f8435859be92255cede8f4
Sara Basile <sarabasile@Saras-Air.home>
2018-03-03 12:01:56 -0500

    added nav links, each link closes any open popups

--- src/js/nav.js
+++ src/js/nav.js
@@ -19,5 +19,45 @@
     $('#islands').removeClass('blur');
   });
 
+  let hidePops = function hidePops() {
+    $('.popup').addClass('hide');
+    $('header').removeClass('bg-white');
+    $('.full-nav').hide();
+    $('.burger').show();
+    $('.nav-x').hide();
+  };
+
+  $('.full-nav li').click(function () {
+    hidePops();
+    if ($(this).is(':contains("whitepaper")')) {
+      $('#whitepaper-popup').removeClass('hide');
+    } else if ($(this).is(':contains("roadmap")')) {
+      $('#roadmap2-popup').removeClass('hide');
+    } else if ($(this).is(':contains("pre-ico")')) {
+      $('#signup-popup').removeClass('hide');
+    } else if ($(this).is(':contains("tokens")')) {
+      $('#tokens-popup').removeClass('hide');
+    } else if ($(this).is(':contains("distribution")')) {
+      $('#distribution-popup').removeClass('hide');
+    } else if ($(this).is(':contains("why")')) {
+      $('#why-now-popup').removeClass('hide');
+    } else if ($(this).is(':contains("market")')) {
+      $('#market-popup').removeClass('hide');
+    } else if ($(this).is(':contains("projections")')) {
+      $('#projections-popup').removeClass('hide');
+    } else if ($(this).is(':contains("team")')) {
+      $('#team-popup').removeClass('hide');
+    } else if ($(this).is(':contains("story")')) {
+      $('#ourstory-popup').removeClass('hide');
+    } else if ($(this).is(':contains("company")')) {
+      $('#company-popup').removeClass('hide');
+    } else if ($(this).is(':contains("advisors")')) {
+      $('#advisors-popup').removeClass('hide');
+    } else if ($(this).is(':contains("contact")')) {
+      $('#contactus-popup').removeClass('hide');
+    } else {
+      return;
+    }
+  });
 
 }());
