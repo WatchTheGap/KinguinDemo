d2696ce9abb279bfe5afa0ae3dbed1afe7274573
Sara Basile <sarabasile@Saras-Air.home>
2018-03-10 12:58:09 -0500

    replaced pieces of about island, added multi step js animation to for the team block.

--- src/js/hovers.js
+++ src/js/hovers.js
@@ -8,5 +8,20 @@
       $(this).find('.social-icons').removeClass('social-hover').find('.social-paths').removeClass('path-hover');
     }
   );
-  
+
+  $('.team-title').hover(
+    function () {
+      $(this).addClass('team-title-hover');
+      $('.team').addClass('team-hover');
+      $('.figures').addClass('figures-hover');
+      $('.shadows').addClass('shadows-hover');
+    },
+    function () {
+      $(this).removeClass('team-title-hover');
+      $('.team').removeClass('team-hover');
+      $('.figures').removeClass('figures-hover');
+      $('.shadows').removeClass('shadows-hover');
+    }
+  );
+
 }());
