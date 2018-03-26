04387b94d39b0c9549570f7e1701b108057a60fa
Sara Basile <sarabasile@Saras-Air.home>
2018-03-26 13:43:08 -0400

    fixed spelling error in helper text.

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -19,7 +19,7 @@
 
   let docsHelper = {
     'title': 'Identification Verification Documents',
-    'text': 'We currently only accept documents in .jpg or .png format. No PDFs. As much as we love them, cat pics are not an acceptable form of identification. Uploading non-essential photographs will greatly delay your approval procecss and puts an unnecessary strain on our hardworking staff. We appreciate your cooperation in adhering to these guidelines.'
+    'text': 'We currently only accept documents in .jpg or .png format. No PDFs. As much as we love them, cat pics are not an acceptable form of identification. Uploading non-essential photographs will greatly delay your approval process and puts an unnecessary strain on our hardworking staff. We appreciate your cooperation in adhering to these guidelines.'
   };
 
 
